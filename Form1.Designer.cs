28ad113665064cfcbfa0e50b43e72cbc53e06e2d
Heallii <90777525+Heallii@users.noreply.github.com>
2021-10-05 23:36:52 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameKopeiki = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameKopeiki
+            // 
+            this.nameKopeiki.Location = new System.Drawing.Point(213, 153);
+            this.nameKopeiki.Name = "nameKopeiki";
+            this.nameKopeiki.Size = new System.Drawing.Size(100, 23);
+            this.nameKopeiki.TabIndex = 0;
+            this.nameKopeiki.TextChanged += new System.EventHandler(this.nameKopeiki_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество копеек:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(456, 60);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Задание 1:\r\nДано натуральное число1≤n≤9999 определяющее стоимость товара в копейк" +
+    "ах. \r\nВыразить стоимость в рублях и копейках, например,\r\n 3 рубля 21 копейка, 15" +
+    " рублей 5 копеек, 1 рубль ровно и т. п.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(212, 216);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(100, 27);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 291);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameKopeiki);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameKopeiki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Задача на копейки"; // заголовок формы
+
+            nameKopeiki.Text = Properties.Settings.Default.n.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)  // кнопка "посчитать"
+        {
+
+            var n = int.Parse(this.nameKopeiki.Text);
+
+            if (1 <= n && n <= 9999)
+            {
+                MessageBox.Show(Logic.MainLogic(n));
+            }
+            else MessageBox.Show("Неверно введена стоимость товара ");
+            Properties.Settings.Default.n = n;
+            Properties.Settings.Default.Save();   // автосохранение
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameKopeiki_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)  // кнопка очистить
+        {
+            nameKopeiki.Text = "";
+        }
+
+        
+    }
+    public class Logic
+    {
+        public static string MainLogic(int n)
+        {
+            int rubles = n / 100, copeys = n % 100;
+            string OutMessage = "";                           // Создаём переменную для вывода сообщения
+            string OutRubles = "";                            // Создаём переменную для хранения рублей 
+            string OutCopeys = "";                            // Создаём переменную для хранения копеек
+            if (rubles > 0)
+            {
+                OutRubles = Convert.ToString(rubles);        // преобразование в строковое представление
+                if (5 <= rubles && rubles <= 20) OutRubles = OutRubles + " рублей "; 
+                else if (rubles % 10 == 1) OutRubles = OutRubles + " рубль ";
+                else if (rubles % 10 == 2 || rubles % 10 == 3 || rubles % 10 == 4) OutRubles = OutRubles + " рубля";
+                else OutRubles = OutRubles + " рублей ";
+            }
+            if (copeys > 0)
+            {
+                OutCopeys = Convert.ToString(copeys);
+                if (5 <= copeys && copeys <= 20) OutCopeys = OutCopeys + " копеек";
+                else if (copeys % 10 == 1) OutCopeys = OutCopeys + " копейка";
+                else if (copeys % 10 == 2 || copeys % 10 == 3 || copeys % 10 == 4) OutCopeys = OutCopeys + " копейки";
+                else OutCopeys = OutCopeys + " копеек";        
+            }
+            OutMessage = OutRubles + "" + OutCopeys;
+            return OutMessage;
+        }
+    }
+
+}
